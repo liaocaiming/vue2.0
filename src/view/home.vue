3e1af3e52b78676c4b813cd03430932dce9610fa
unknown <CaimingLiao@snsshop.net>
2017-08-03 17:56:15 +0800

    home

--- src/view/home.vue
+++ src/view/home.vue
@@ -1,10 +1,27 @@
 <template>
   <section>
     <h1>我是首页</h1>
+    <div v-for="(value, key) in useInfo" :key="key">{{value}}</span></div>
+    <div @click="changeObj">改变对象</div>
   </section>
 </template>
 <script>
-  console.log(6666)
+export default {
+  data () {
+    return {
+      useInfo: {
+        name: 'liaocaiming',
+        age: '18'
+      }
+    }
+  },
+  methods: {
+    changeObj () {
+      console.log(888)
+      this.useInfo = Object.assign({sex: 'name'}, this.useInfo)
+    }
+  }
+}
 </script>
 <style>
   body {
